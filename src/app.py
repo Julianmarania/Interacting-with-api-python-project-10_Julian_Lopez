2f984ffe931e1e0d2fc294fe0595330074b8eb63
[julian] <[julianmarania27@gmail.com]>
2025-02-21 12:35:58 +0100

    Step 3

--- src/app.py
+++ src/app.py
@@ -2,6 +2,12 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy as sp
 
 # load the .env file variables
 load_dotenv()
+
+# Leer las variables CLIENT_ID y CLIENT_SECRET desde el archivo .env
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
